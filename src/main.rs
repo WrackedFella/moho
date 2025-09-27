2bf06459277696538363c73386410a85f5902569
Moore, Justin <jusmoo02@roberthalf.com>
2025-09-27 11:31:21 -0400

    removed most spheres for testing

--- src/main.rs
+++ src/main.rs
@@ -231,76 +231,76 @@ fn random_scene(world: &mut World) {
         },
     );
     world.push((sphere,));
-    for a in -11..11 {
-        for b in -11..11 {
-            let choose_mat = rng().random::<f32>();
-            let center = Vec3::new(
-                a as f32 + 0.9f32 * rng().random::<f32>(),
-                0.2f32,
-                b as f32 + 0.9f32 * rng().random::<f32>(),
-            );
-            if vector_length(center - Vec3::new(4f32, 0.2f32, 0f32)) > 0.9f32 {
-                if choose_mat < 0.8f32 {
-                    // diffuse
-                    let sphere = Sphere::new(
-                        center,
-                        0.2f32,
-                        MaterialType::Lambertian {
-                            albedo: Vec3::new(
-                                rng().random::<f32>() * rng().random::<f32>(),
-                                rng().random::<f32>() * rng().random::<f32>(),
-                                rng().random::<f32>() * rng().random::<f32>(),
-                            ),
-                        },
-                    );
-                    world.push((sphere,));
-                } else if choose_mat < 0.95f32 {
-                    // metal
-                    let sphere = Sphere::new(
-                        center,
-                        0.2f32,
-                        MaterialType::Metal {
-                            albedo: Vec3::new(
-                                0.5f32 * (1f32 + rng().random::<f32>()),
-                                0.5f32 * (1f32 + rng().random::<f32>()),
-                                0.5f32 * (1f32 + rng().random::<f32>()),
-                            ),
-                            fuzz: 0.5f32 * rng().random::<f32>(),
-                        },
-                    );
-                    world.push((sphere,));
-                } else {
-                    // glass
-                    let sphere = Sphere::new(
-                        center,
-                        0.2f32,
-                        MaterialType::Dielectric { ref_indx: 1.5f32 },
-                    );
-                    world.push((sphere,));
-                }
-            }
-        }
-    }
+    // for a in -11..11 {
+    //     for b in -11..11 {
+    //         let choose_mat = rng().random::<f32>();
+    //         let center = Vec3::new(
+    //             a as f32 + 0.9f32 * rng().random::<f32>(),
+    //             0.2f32,
+    //             b as f32 + 0.9f32 * rng().random::<f32>(),
+    //         );
+    //         if vector_length(center - Vec3::new(4f32, 0.2f32, 0f32)) > 0.9f32 {
+    //             if choose_mat < 0.8f32 {
+    //                 // diffuse
+    //                 let sphere = Sphere::new(
+    //                     center,
+    //                     0.2f32,
+    //                     MaterialType::Lambertian {
+    //                         albedo: Vec3::new(
+    //                             rng().random::<f32>() * rng().random::<f32>(),
+    //                             rng().random::<f32>() * rng().random::<f32>(),
+    //                             rng().random::<f32>() * rng().random::<f32>(),
+    //                         ),
+    //                     },
+    //                 );
+    //                 world.push((sphere,));
+    //             } else if choose_mat < 0.95f32 {
+    //                 // metal
+    //                 let sphere = Sphere::new(
+    //                     center,
+    //                     0.2f32,
+    //                     MaterialType::Metal {
+    //                         albedo: Vec3::new(
+    //                             0.5f32 * (1f32 + rng().random::<f32>()),
+    //                             0.5f32 * (1f32 + rng().random::<f32>()),
+    //                             0.5f32 * (1f32 + rng().random::<f32>()),
+    //                         ),
+    //                         fuzz: 0.5f32 * rng().random::<f32>(),
+    //                     },
+    //                 );
+    //                 world.push((sphere,));
+    //             } else {
+    //                 // glass
+    //                 let sphere = Sphere::new(
+    //                     center,
+    //                     0.2f32,
+    //                     MaterialType::Dielectric { ref_indx: 1.5f32 },
+    //                 );
+    //                 world.push((sphere,));
+    //             }
+    //         }
+    //     }
+    // }
     world.push((Sphere::new(
         Vec3::new(0f32, 1f32, 0f32),
         1f32,
         MaterialType::Dielectric { ref_indx: 1.5f32 },
     ),));
-    world.push((Sphere::new(
-        Vec3::new(-4f32, 1f32, 0f32),
-        1f32,
-        MaterialType::Lambertian {
-            albedo: Vec3::new(0.4f32, 0.2f32, 0.1f32),
-        },
-    ),));
-    world.push((Sphere::new(
-        Vec3::new(4f32, 1f32, 0f32),
-        1f32,
-        MaterialType::Metal {
-            albedo: Vec3::new(0.7f32, 0.6f32, 0.5f32),
-            fuzz: 0.0f32,
-        },
-    ),));
+    // world.push((Sphere::new(
+    //     Vec3::new(-4f32, 1f32, 0f32),
+    //     1f32,
+    //     MaterialType::Lambertian {
+    //         albedo: Vec3::new(0.4f32, 0.2f32, 0.1f32),
+    //     },
+    // ),));
+    // world.push((Sphere::new(
+    //     Vec3::new(4f32, 1f32, 0f32),
+    //     1f32,
+    //     MaterialType::Metal {
+    //         albedo: Vec3::new(0.7f32, 0.6f32, 0.5f32),
+    //         fuzz: 0.0f32,
+    //     },
+    // ),));
     println!("World Generated");
 }
 
