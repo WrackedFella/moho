c14876c20aaa70297d9ed21cde5f69ccfd6b99a1
Moore, Justin <jusmoo02@roberthalf.com>
2025-10-13 13:43:33 -0400

    style changes to start menu

--- moho_ui/src/menus/start.rs
+++ moho_ui/src/menus/start.rs
@@ -43,11 +43,8 @@ impl Menu for StartMenu {
                 let paint_decor = |ui: &mut egui::Ui, resp: &egui::Response| {
                     if resp.hovered() {
                         let r = resp.rect;
-                        ui.painter().rect_filled(
-                            r,
-                            4.0,
-                            ui.visuals().widgets.active.bg_fill.linear_multiply(1.05),
-                        );
+                        let hover_color = egui::Color32::from_rgba_premultiplied(150, 150, 150, 100); // Light grey highlight
+                        ui.painter().rect_filled(r, 4.0, hover_color);
                     }
                 };
 
